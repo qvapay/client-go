3d9a783aa3e5e81110b12b5190a5a14b2317ecfb
wilobs <wilson.bolanossoto@gmail.com>
2023-02-18 19:14:00 -0500

    ref: Change Debug estrategy to io.Writer

--- client.go
+++ client.go
@@ -16,7 +16,7 @@ type apiClient struct {
 	server string
 	client *http.Client
 	//optional for debuging
-	debug bool
+	debug io.Writer
 }
 
 // HttpRequestDoer defines methods for a http client.
@@ -30,7 +30,7 @@ type APIClientOptions struct {
 	//optional, defaults to http.DefaultClient
 	HttpClient *http.Client
 	//optional for debuging
-	Debug bool
+	Debug io.Writer
 	// Server url to use
 	Server string
 }
@@ -49,10 +49,10 @@ func NewAPIClient(options APIClientOptions) APIClient {
 
 	// Logging consideration
 	c.client.Transport = &headersRoundTripper{
-		next: &loggingRoundTripper{
-			next:    http.DefaultTransport,
-			logger:  os.Stdout,
-			enabled: options.Debug,
+		next: &debugRoundTripper{
+			next:   http.DefaultTransport,
+			logger: os.Stdout,
+			Debug:  options.Debug,
 		},
 	}
 
@@ -76,22 +76,23 @@ func (t authRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
 	return t.next.RoundTrip(r)
 }
 
-type loggingRoundTripper struct {
-	next    http.RoundTripper
-	logger  io.Writer
-	enabled bool
+type debugRoundTripper struct {
+	next   http.RoundTripper
+	logger io.Writer
+	Debug  io.Writer
 }
 
-func (l *loggingRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
+func (l *debugRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
 	// Here we can set a fancy Logging implementation
-	// Request Dump
-	if l.enabled {
+	if l.Debug != nil {
+		// Request Dump
 		dump, err := httputil.DumpRequestOut(r, true)
 		if err != nil {
 			fmt.Printf("error dumping HTTP request: %s", err.Error())
 			return l.next.RoundTrip(r)
 		}
-		log.Println("Request:", string(dump))
+		fmt.Fprintln(l.Debug, string(dump))
+		fmt.Fprintln(l.Debug)
 
 		// Response Dump
 		resp, err := l.next.RoundTrip(r)
@@ -100,7 +101,8 @@ func (l *loggingRoundTripper) RoundTrip(r *http.Request) (*http.Response, error)
 			fmt.Printf("error dumping HTTP reponse: %s", err.Error())
 			return l.next.RoundTrip(r)
 		}
-		log.Println("Response:", string(dump))
+		fmt.Fprintln(l.Debug, string(dump))
+		fmt.Fprintln(l.Debug)
 
 		return resp, nil
 	}
@@ -118,32 +120,6 @@ func (l *headersRoundTripper) RoundTrip(r *http.Request) (*http.Response, error)
 
 	return l.next.RoundTrip(r)
 }
-
-// // dumpResponse writes the raw response data to the debug output, if set, or
-// // standard error otherwise.
-// func (c *apiClient) dumpResponse(resp *http.Response) {
-// 	// ignore errors dumping response - no recovery from this
-// 	responseDump, err := httputil.DumpResponse(resp, true)
-// 	if err != nil {
-// 		log.Fatalf("dumpResponse: " + err.Error())
-// 	}
-// 	fmt.Fprintln(c.debug, string(responseDump))
-// 	fmt.Fprintln(c.debug)
-// }
-
-// func apiCallDebugger(req *http.Request, debug io.Writer) error {
-// 	req.Header.Add("content-type", "application/json")
-// 	req.Header.Add("User-Agent", "qvapay-go")
-// 	if debug != nil {
-// 		requestDump, err := httputil.DumpRequestOut(req, true)
-// 		if err != nil {
-// 			return fmt.Errorf("error dumping HTTP request: %v", err)
-// 		}
-// 		fmt.Fprintln(debug, string(requestDump))
-// 		fmt.Fprintln(debug)
-// 	}
-// 	return nil
-// }
 
 func DrainBody(respBody io.ReadCloser) {
 	// Callers should close resp.Body when done reading from it.
