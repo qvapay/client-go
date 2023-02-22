560d9aa9916ac498a8c8ab5ed52bf298ae2298b6
wilobs <wilson.bolanossoto@gmail.com>
2023-02-22 00:30:48 -0500

    test: Add some tests in Login and Logout funcs

--- api_auth_test.go
+++ api_auth_test.go
@@ -45,7 +45,7 @@ func TestLogin(t *testing.T) {
 		assert.Equal(t, want, login.Me.Name)
 	})
 
-	t.Run("sad path - bad Login payload values", func(t *testing.T) {
+	t.Run("sad path - Empty Login payload values", func(t *testing.T) {
 		data := `
 	{
 		"accessToken":"387003",
@@ -75,10 +75,8 @@ func TestLogin(t *testing.T) {
 
 		assert.Equal(t, ErrCreateReq, err)
 	})
-}
 
-func TestLogout(t *testing.T) {
-	t.Run("sad path - test http server successfully logout", func(t *testing.T) {
+	t.Run("sad path - Wrong Credentials values", func(t *testing.T) {
 		data := `
 	{
 		"accessToken":"387003",
@@ -93,8 +91,92 @@ func TestLogout(t *testing.T) {
 	`
 
 		svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(http.StatusCreated)
+			w.WriteHeader(http.StatusUnprocessableEntity)
+			fmt.Fprintf(w, data)
+		}))
 
+		defer svr.Close()
+		api := NewAPIClient(APIClientOptions{
+			Server: svr.URL,
+		})
+
+		_, err := api.Login(context.TODO(), LoginRequest{
+			Email:    "bademail@gial.com",
+			Password: "1q2w3e4r5t",
+		})
+
+		assert.Equal(t, ErrUnsuccessfulRes, err)
+	})
+
+	t.Run("sad path - Unmarshable response Body", func(t *testing.T) {
+		data := `
+	{
+		{"accessToken":"387003",
+		"token_type":"Bearer",
+		"me":{
+			"uuid":"sd-sd-s-sd-sd",
+			"username":"user@gmail.com",
+			"name":"QvaPayUser",
+			"lastname":"Paymenton"
+		}
+	}
+	`
+
+		svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, data)
+		}))
+
+		defer svr.Close()
+		api := NewAPIClient(APIClientOptions{
+			Server: svr.URL,
+		})
+
+		_, err := api.Login(context.TODO(), LoginRequest{
+			Email:    "user@gmail.com",
+			Password: "1q2w3e4r5t",
+		})
+
+		assert.Equal(t, ErrCreateRes, err)
+	})
+}
+
+func TestLogout(t *testing.T) {
+	t.Run("sad path - User wasn't Logged in Before", func(t *testing.T) {
+		data := `
+	{
+		"message":"successfully logout"
+	}
+	`
+		authUser.AccessToken = ""
+
+		svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusCreated)
+			fmt.Fprintf(w, data)
+		}))
+
+		defer svr.Close()
+		api := NewAPIClient(APIClientOptions{
+			Server: svr.URL,
+		})
+
+		logout, err := api.Logout(context.TODO())
+		if err != nil {
+			assert.Equal(t, ErrCreateReq, err)
+		}
+
+		assert.Empty(t, logout)
+	})
+
+	t.Run("happy path - test http server successfully logout", func(t *testing.T) {
+		data := `
+	{
+		"message":"successfully logout"
+	}
+	`
+		authUser.AccessToken = "1q2w3e4r5t"
+
+		svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusCreated)
 			fmt.Fprintf(w, data)
 		}))
 
@@ -107,8 +189,37 @@ func TestLogout(t *testing.T) {
 		if err != nil {
 			t.Fatalf(err.Error())
 		}
+
 		assert.Equal(t, nil, err)
+		assert.NotEmpty(t, logout)
+		assert.Equal(t, "", authUser.AccessToken)
+	})
+
+	t.Run("sad path - Unmarshable response Body", func(t *testing.T) {
+		data := `
+	{{
+		"message":"successfully logout"
+	}
+	`
+		authUser.AccessToken = "1q2w3e4r5t"
+
+		svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusCreated)
+			fmt.Fprintf(w, data)
+		}))
+
+		defer svr.Close()
+		api := NewAPIClient(APIClientOptions{
+			Server: svr.URL,
+		})
+
+		logout, err := api.Logout(context.TODO())
+		if err != nil {
+			assert.Equal(t, ErrCreateRes, err)
+		}
+
 		assert.Empty(t, logout)
+		assert.NotEqual(t, "", authUser.AccessToken)
 	})
 
 }
